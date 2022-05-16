860feb926adab472aa53d7c5db6e71b5a4c59b58
ourmetaverse <tingzhao.ytz@antgroup.com>
2022-05-16 19:31:37 +0800

    chore: update index btn style

--- src/components/IndexBtn.tsx
+++ src/components/IndexBtn.tsx
@@ -20,6 +20,28 @@ const Component: React.FC = ({ children }) => {
         align-items:center;
         justify-content:center;
         cursor:pointer;
+
+        position: relative;
+        overflow: hidden;
+        &:after {
+          background: #fff;
+          content: "";
+          height: 155px;
+          left: -75px;
+          opacity: .2;
+          position: absolute;
+          top: -50px;
+          transform: rotate(35deg);
+          width: 50px;
+          z-index: -10;
+        }
+        &:hover {
+          &:after {
+            left: 120%;
+            transition: all 1000ms cubic-bezier(0.19, 1, 0.22, 1);
+          }
+        }
+
         ${mobile} {
           width:132x;
           height:50px;
